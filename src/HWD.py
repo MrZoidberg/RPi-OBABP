11bf6470f1f90807a70e4443ae4c8b3174d6ba5b
Mikhail Merkulov <Mihail.Merkulov@gmail.com>
2016-03-29 01:00:57 +0300

    New script initial commit

---
+++ src/HWD.py
@@ -0,0 +1,72 @@
+import RPi.GPIO as GPIO
+
+
+class HDW(object):
+
+    def __init__(self,):
+        GPIO.setmode(GPIO.BOARD)
+        self._statusLed = -1
+        self._powerLed = -1
+        self._playButton = -1
+        self._volumeUpButton = -1
+        self._volumeDownButton = -1
+        self.flashes = dict()
+
+    @property
+    def statusLed(self):
+        return self._statusLed
+
+    @property
+    def powerLed(self):
+        return self._powerLed
+
+    @property
+    def playButton(self):
+        return self._playButton
+
+    @property
+    def volumeUpButton(self):
+        return self._volumeUpButton
+
+    @property
+    def volumeDownButton(self):
+        return self._volumeDownButton
+
+    def setStatusLed(self, _led):
+        self._statusLed = _led
+
+    def setPowerLed(self, _led):
+        self._powerLed = _led
+
+    def setPlayButton(self, _button):
+        self._playButton = _button
+
+    def setVolumeDownButton(self, _button):
+        self._volumeDownButton = _button
+
+    def setVolumeUpButton(self, _button):
+        self._volumeUpButton = _button
+
+    def flashLed(self, led, speed, time):
+        pwm = GPIO.PWM(led, speed)
+        pwm.start(time)
+
+    def stopFlash(self, led):
+        pwm = self.flashes[led]
+        if pwm:
+            pwm.stop()
+            del self.flashes[led]
+
+    def setup(self):
+        leds = [self._powerLed, self._statusLed]
+        buttons = [self._playButton, self._volumeUpButton,
+                   self._volumeDownButton]
+
+        GPIO.setup(leds, GPIO.OUT)
+        GPIO.setup(buttons, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        GPIO.output(self._powerLed, GPIO.HIGH)
+        GPIO.output(self._statusLed, GPIO.LOW)
+
+    def cleanup(self):
+        GPIO.cleanup()
---
+++ src/obabp2.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import HWD
+
+def main():
+    try:
+        hwd = HWD()
+        hwd.setStatusLed(18)
+        hwd.setPowerLed(16)
+        hwd.setPlayButton(11)
+        hwd.setVolumeUpButton(13)
+        hwd.setVolumeDownButton(15)
+        hwd.setup()
+
+        hwd.flashLed(hwd.powerLed, 2, 50)
+        hwd.flashLed(hwd.statusLed, 2, 50)
+
+        raw_input("Press Enter to exit...")
+
+    except KeyboardInterrupt:
+        print "exiting from button"
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+
+    hwd.cleanup()
+    # Restart application
+    os.execl('runme.sh', '')
+
+if __name__ == '__main__':
+    main()
