1a81c3ef64fbc55293839f7f20017fd14b36c38b
Mikhail Merkulov <Mihail.Merkulov@gmail.com>
2016-03-28 23:29:34 +0300

    button test

--- src/button.py
+++ src/button.py
@@ -7,26 +7,29 @@ def main():
     button = 11
     pressed = False
 
+    GPIO.cleanup()
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(led, GPIO.OUT)
-    GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.output(led, GPIO.HIGH)
 
+    GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     print("init done")
-
-    while True:
-        if GPIO.input(button) is False:
-            sleep(0.01)
-            if not pressed:
-                print("button pressed")
-                GPIO.output(led, GPIO.LOW)
-                pressed = True
-        elif pressed:
-            print("button released")
-            GPIO.output(led, GPIO.HIGH)
-            pressed = False
-        sleep(0.1)
-
+    try:
+        while True:
+            if GPIO.input(button) is False:
+                sleep(0.01)
+                if not pressed:
+                    print("button pressed")
+                    GPIO.output(led, GPIO.LOW)
+                    pressed = True
+            elif pressed:
+                print("button released")
+                GPIO.output(led, GPIO.HIGH)
+                pressed = False
+            sleep(0.1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
