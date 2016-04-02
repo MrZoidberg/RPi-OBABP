bf789e679d90add9b7ff4613cdeb24276c0501e6
Mikhail Merkulov <Mihail.Merkulov@gmail.com>
2016-04-02 20:02:17 +0300

    dev

--- src/obabp2.py
+++ src/obabp2.py
@@ -75,9 +75,9 @@ def main():
                 print "usb drive removed"
                 hwd.stopFlash(hwd.statusLed)
 
-            print player.getStats()["songs"]
             if player.getStats()["songs"] == 0:
                 if noSongsLed is False:
+                    print "no songs found"
                     hwd.flashLed(hwd.statusLed, 0.5, 50)
                     noSongsLed = True
             elif noSongsLed is True:
